be2b09e006189f637be0891553ed77e207f0c7e5
SolanoJason <pajason2000@outlook.com>
2024-07-26 12:47:59 -0500

    feat: add pre-commit configuration and update dependencies
    - Added .pre-commit-config.yaml with hooks for code quality and formatting checks.
    - Updated .flake8 to ignore F401 and F403 and added no-accept-encodings rule.
    - Removed DEBUG from base settings and added it to development settings.
    - Added ALLOWED_HOSTS to development settings and created new staging settings file.
    - Updated pyproject.toml with flake8-builtins, flake8-coding, pre-commit, and isort.
    - Added isort configuration with black profile.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -28,8 +28,6 @@ sys.path.insert(0, str(BASE_DIR / 'apps'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- project_name/settings/development.py
+++ project_name/settings/development.py
@@ -1,5 +1,9 @@
 from .base import *
 
+DEBUG = True
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]"]  # Default when debug is True
